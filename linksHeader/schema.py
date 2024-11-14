213dd9673de521b043592de080e87e513e73acea
harlock024 <hadith024@gmail.com>
2024-11-13 22:36:42 -0600

    feat: linkheader schema update delete

--- linksHeader/schema.py
+++ linksHeader/schema.py
@@ -48,5 +48,53 @@ class CreateLinksHeader(graphene.Mutation):
                 header = linkHeader.header
             )
 
+class UpdateLinksHeader(graphene.Mutation):
+    idLinkHeader = graphene.Int(required=True)
+    title = graphene.String(required=True)
+    icon = graphene.String()
+    link = graphene.String()
+    header = graphene.Int()
+
+    class Arguments:
+        idLinkHeader = graphene.Int(required=True)
+        title = graphene.String(required=True)
+        icon = graphene.String()
+        link = graphene.String()
+        header = graphene.Int()
+    def mutate(self,info,title,icon,link,header):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('Login to create a link')
+
+        header = Header.objects.get(id=header)
+        linkHeader = LinksHeader.objects.get(id=idLinkHeader)
+        linkHeader.title = title
+        linkHeader.icon = icon
+        linkHeader.link = link
+        linkHeader.header = header
+        linkHeader.save()
+        return UpdateLinksHeader(
+            title = linkHeader.title,
+            icon = linkHeader.icon,
+            link = linkHeader.link,
+            header = linkHeader.header
+        )
+class DeleteLinksHeader(graphene.Mutation):
+    succes = graphene.Boolean()
+
+    class Arguments:
+        idLinkHeader = graphene.Int(required=True)
+
+    def mutate(self,info,idLinkHeader):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('Login to delete a link')
+        linkHeader = LinksHeader.objects.get(id=idLinkHeader)
+        linkHeader.delete()
+        return DeleteLinksHeader(
+            succes = True
+        )
 class Mutation(graphene.ObjectType):
     create_linksHeader = CreateLinksHeader.Field()
+    update_linksHeader = UpdateLinksHeader.Field()
+    delete_linksHeader = DeleteLinksHeader.Field()
