6c46e6d615caa9c6e87ac6a0f5bc15ee1225e66c
harlock024 <hadith024@gmail.com>
2024-11-13 21:55:36 -0600

    feat: languajes schema update delete

--- languajes/schema.py
+++ languajes/schema.py
@@ -32,5 +32,42 @@ class CreateLanguages(graphene.Mutation):
             posted_by=language.posted_by
         )
 
+class UpdateLanguages(graphene.Mutation):
+        idLanguage = graphene.Int()
+        name = graphene.String(required=True)
+        class Arguments:
+            idLanguage = graphene.Int()
+            name = graphene.String(required=True)
+
+        def mutate(self, info , idLanguage,name):
+            user = info.context.user
+            if user.is_anonymous:
+                raise GraphQLError('Login to update a languages')
+            language = languages.objects.get(id=idLanguage)
+            language.name = name
+            language.save()
+
+            return UpdateLanguages(
+                idLanguage=language.id,
+                name=language.name
+            )
+
+class Deletelanguages(graphene.Mutation):
+        success = graphene.Boolean()
+
+        class Arguments:
+            idLanguage = graphene.Int()
+
+        def mutate(self, info , idLanguage):
+            user = info.context.user
+            if user.is_anonymous:
+                raise GraphQLError('Login to delete a languages')
+            language = languages.objects.get(id=idLanguage)
+            language.delete()
+
+            return Deletelanguages(success=True)
+
 class Mutation(graphene.ObjectType):
     create_languages = CreateLanguages.Field()
+    update_languages = UpdateLanguages.Field()
+    delete_languages = Deletelanguages.Field()
