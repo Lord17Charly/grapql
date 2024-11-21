67d74a3997b901456bb15d28f3478242835761b8
harlock024 <hadith024@gmail.com>
2024-11-21 00:28:27 -0600

    skill  test schema

--- skills/schema.py
+++ skills/schema.py
@@ -1,18 +1,29 @@
-from django.urls.conf import URLPattern
 import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
 from .models import Skill
+from django.db.models import Q
 
-class skillsType(DjangoObjectType):
+class SkillType(DjangoObjectType):
     class Meta:
         model = Skill
 
 class Query(graphene.ObjectType):
-    skills = graphene.List(skillsType)
+    skills = graphene.List(SkillType)
+    skills_by_id = graphene.Field(SkillType, id=graphene.Int(required=True))
 
     def resolve_skills(self, info):
         return Skill.objects.all()
+
+    def resolve_skills_by_id(self, info, id, **Kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('Log in to see your skills')
+        # Se devuelve la skill con el filtro adecuado
+        skill = Skill.objects.filter(posted_by=user, id=id).first()
+        if not skill:
+            raise GraphQLError('Skill not found')
+        return skill
 
 class CreateSkill(graphene.Mutation):
     name = graphene.String(required=True)
@@ -21,7 +32,7 @@ class CreateSkill(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
 
-    def  mutate(self, info,name):
+    def mutate(self, info, name):
         user = info.context.user
         if user.is_anonymous:
             raise GraphQLError('Login to create a skill')
@@ -53,9 +64,9 @@ class UpdateSkill(graphene.Mutation):
         skill.save()
         return UpdateSkill(idSkill=skill.id, name=skill.name)
 
-
 class DeleteSkill(graphene.Mutation):
     success = graphene.Boolean()
+
     class Arguments:
         idSkill = graphene.Int(required=True)
 
--- skills/tests.py
+++ skills/tests.py
@@ -1,3 +1,141 @@
+import json
 from django.test import TestCase
+from graphene_django.utils.testing import GraphQLTestCase
+from django.contrib.auth import get_user_model
+from mixer.backend.django import mixer
+from .models import Skill
 
-# Create your tests here.
+class SkillTestCase(GraphQLTestCase):
+    GRAPHQL_URL = "/graphql/"
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            password="testpassword"
+        )
+
+        response_token = self.query(
+            '''
+            mutation TokenAuth($username: String!, $password: String!) {
+                tokenAuth(username: $username, password: $password) {
+                    token
+                }
+            }
+            ''',
+            variables={'username': 'testuser', 'password': 'testpassword'}
+        )
+        content_token = json.loads(response_token.content)
+        self.token = content_token['data']['tokenAuth']['token']
+        self.headers = {"HTTP_AUTHORIZATION": f"JWT {self.token}"}
+
+        # Crear una habilidad para las pruebas
+        self.skill = mixer.blend(
+            Skill,
+            name="JavaScript",
+            posted_by=self.user
+        )
+
+    def test_query_skill_by_id(self):
+        QUERY_SKILL_BY_ID = '''
+        query GetSkillById($id: Int!) {
+            skillsById(id: $id) {
+                name
+                postedBy {
+                    username
+                }
+            }
+        }
+        '''
+        variables = {"id": self.skill.id}
+
+        response = self.client.post(
+            self.GRAPHQL_URL,
+            data=json.dumps({
+                "query": QUERY_SKILL_BY_ID,
+                "variables": variables
+            }),
+            content_type="application/json",
+            **self.headers
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['skillsById']['name'], "JavaScript")
+        self.assertEqual(content['data']['skillsById']['postedBy']['username'], "testuser")
+
+
+    def test_create_skill(self):
+        CREATE_SKILL_MUTATION = '''
+        mutation CreateSkill($name: String!) {
+            createSkill(name: $name) {
+                name
+            }
+        }
+        '''
+        variables = {"name": "Python"}
+
+        response = self.client.post(
+            self.GRAPHQL_URL,
+            data=json.dumps({
+                "query": CREATE_SKILL_MUTATION,
+                "variables": variables
+            }),
+            content_type="application/json",
+            **self.headers
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['createSkill']['name'], "Python")
+
+    def test_update_skill(self):
+        UPDATE_SKILL_MUTATION = '''
+        mutation UpdateSkill($idSkill: Int!, $name: String!) {
+            updateSkill(idSkill: $idSkill, name: $name) {
+                idSkill
+                name
+            }
+        }
+        '''
+        variables = {
+            "idSkill": self.skill.id,
+            "name": "Django"
+        }
+
+        response = self.client.post(
+            self.GRAPHQL_URL,
+            data=json.dumps({
+                "query": UPDATE_SKILL_MUTATION,
+                "variables": variables
+            }),
+            content_type="application/json",
+            **self.headers
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['updateSkill']['name'], "Django")
+
+    def test_delete_skill(self):
+        DELETE_SKILL_MUTATION = '''
+        mutation DeleteSkill($idSkill: Int!) {
+            deleteSkill(idSkill: $idSkill) {
+                success
+            }
+        }
+        '''
+        variables = {"idSkill": self.skill.id}
+
+        response = self.client.post(
+            self.GRAPHQL_URL,
+            data=json.dumps({
+                "query": DELETE_SKILL_MUTATION,
+                "variables": variables
+            }),
+            content_type="application/json",
+            **self.headers
+        )
+
+        content = json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertTrue(content['data']['deleteSkill']['success'])
